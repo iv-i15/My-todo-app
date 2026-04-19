58934ced2eeacca121ac02e4fa06bc008f594b99
iv-i15 <ivet.antova@abv.bg>
2026-04-19 23:19:49 +0300

    Update TodoItem.jsx

--- TodoItem.jsx
+++ TodoItem.jsx
@@ -1,29 +1,27 @@
 function TodoItem({todo, onToggle, onDelete}){
     return(
         <li>
-            <div className="todo_left">
-                <input 
-                    type="checkbox" 
-                    checked={todo.completed}
-                    onChange={()=>{
-                        onToggle(todo.id)
-                    }}
-                />
-
-                {todo.text}
-
-                {todo.completed && todo.completedAt && (
-                <small>
-                    {" "}
-                    ({new Date(todo.completedAt).toLocaleString()})            </small>
-                )}
-
-                <button onClick={() => {
-                    onDelete(todo.id)
+            <input 
+                type="checkbox" 
+                checked={todo.completed}
+                onChange={()=>{
+                    onToggle(todo.id)
                 }}
-                >Delete</button>
-            </div>
+            />
 
+            {todo.text}
+
+            {todo.completed && todo.completedAt && (
+            <small>
+                {" "}
+                ({new Date(todo.completedAt).toLocaleString()})   
+            </small>
+             )}
+
+            <button onClick={() => {
+                onDelete(todo.id)
+            }}
+            >Delete</button>
         </li>
     )
 }
